3dc3df2dde1fc5e440edf628b1ea2f73c5cf5197
Olukeye <seunolukeye2000@gmail.com>
2022-01-16 02:02:34 +0100

    ObjectId in Schema

--- model/posts.js
+++ model/posts.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
+const  { ObjectId } = mongoose.Schema;
 
 
 const postSchema = new mongoose.Schema({
     userId: {
         type:ObjectId,
         ref: 'User',
-        // required:true
+        required:true
     },
     description: {
         type:String,
