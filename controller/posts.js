9480839d3193071ea1fa40ceac81027e6d948e45
Olukeye <seunolukeye2000@gmail.com>
2022-01-18 23:49:05 +0100

    edited

--- controller/posts.js
+++ controller/posts.js
@@ -1,16 +1,13 @@
-const express = require('express')
 const Post = require('../model/posts')
-const router = express.Router();
 
 
 exports.createPost = async(req, res ) => {
-    // const post = new Post(req.body.description)
     const post = await new Post(
         req.body
     )
     try{
         if(req.body.description.length === 0) {
-            res.status(300).json("Post cant be empty!");
+            res.status(300).json("Post Cant Be Empty!");
             
         } else {
         const savePost = await post.save();
