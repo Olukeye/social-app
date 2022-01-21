0d1d249ae99cb34d934f289259fc374d63ab8dd1
Olukeye <seunolukeye2000@gmail.com>
2022-01-21 23:55:11 +0100

    updatePost modify

--- controller/posts.js
+++ controller/posts.js
@@ -23,9 +23,9 @@ exports.createPost = async(req, res ) => {
 exports.updatePost = async(req, res) => {
     try{
         const post = await Post.findById(req.params.id);
-        if(post.userId === req.body.userId){
-             await post.updateOne({$set: req.body}, {new: true});
-            res.status(200).json("corrected")
+        if(post.userId == req.body.userId){
+             await post.updateOne({$set: req.body});
+            res.status(200).json("updated successfully!!")
         }else {
             res.status(403).json("sorry you cant perform this!")
         }
