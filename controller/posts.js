6018e9db770c38a66dc1a536be682637b7e58752
Olukeye <seunolukeye2000@gmail.com>
2022-01-22 01:22:59 +0100

    delete a post api

--- controller/posts.js
+++ controller/posts.js
@@ -6,7 +6,7 @@ exports.createPost = async(req, res ) => {
         req.body
     )
     try{
-        if(req.body.description.length === 0) {
+        if(req.body.description.length == 0) {
             res.status(300).json("Post Cant Be Empty!");
             
         } else {
@@ -18,33 +18,32 @@ exports.createPost = async(req, res ) => {
     }
 }
 
+exports.updatePost = async(req, res) => {   
+
+    const post = await Post.findById(req.params.id);
+    if(post.userId == req.body.userId) {
+        try{
+             await post.updateOne({ $set:req.body })
+            res.status(200).json("updated successfully!!")
+        }catch(err){
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("sorry you cant perform this!")
+    }
+}
 
 // update a post
-exports.updatePost = async(req, res) => {
+exports.deletePost = async(req, res) => {
     try{
         const post = await Post.findById(req.params.id);
         if(post.userId == req.body.userId){
-             await post.updateOne({$set: req.body});
-            res.status(200).json("updated successfully!!")
-        }else {
+             await post.deleteOne();
+            res.status(200).json("Post delete successfully!!")
+        } else {
             res.status(403).json("sorry you cant perform this!")
         }
     } catch(err) {
         res.status(500).json(err);
     }
 }
-
-// exports.updatePost = async(req, res) => {
-//     if(req.userId === req.body.userId){
-//         try{
-//             const update = await Post.findByIdAndUpdate(req.params.id, {
-//                 $set:req.body
-//             })
-//             res.status(200).json(update)
-//         }catch(err){
-//             res.status(500).json(err);
-//         }
-//     } else {
-//         res.status(403).json("sorry you cant perform this!")
-//     }
-// }
--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = new express.Router();
 
 
-const {createPost, updatePost} = require('../controller/posts');
+const {createPost, updatePost, deletePost} = require('../controller/posts');
 
 router.post('/posts', createPost)
 router.put('/post/:id', updatePost )
+router.delete('/post/:id', deletePost)
 
 
 
